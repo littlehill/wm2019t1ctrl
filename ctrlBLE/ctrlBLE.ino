2d1b6e1e64ffa48e31ecf958bad6a8466f3367b9
jelinekk <jelinekk@btlnet.com>
2019-11-19 16:51:03 +0100

    Added control part of BLE receiving information

---
+++ ctrlBLE/ctrlBLE.ino
@@ -0,0 +1,122 @@
+/*
+  LED Control
+
+  This example scans for BLE peripherals until one with the advertised service
+  "19b10000-e8f2-537e-4f6c-d104768a1214" UUID is found. Once discovered and connected,
+  it will remotely control the BLE Peripheral's LED, when the button is pressed or released.
+
+  The circuit:
+  - Arduino MKR WiFi 1010, Arduino Uno WiFi Rev2 board, Arduino Nano 33 IoT,
+    Arduino Nano 33 BLE, or Arduino Nano 33 BLE Sense board.
+  - Button with pull-up resistor connected to pin 2.
+
+  You can use it with another board that is compatible with this library and the
+  Peripherals -> LED example.
+
+  This example code is in the public domain.
+*/
+
+#include <ArduinoBLE.h>
+#define DATA_LENGTH 6
+// variables for button
+const int buttonPin = 2;
+int oldButtonState = LOW;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  // configure the button pin as input
+  pinMode(buttonPin, INPUT);
+
+  // initialize the BLE hardware
+  BLE.begin();
+
+  Serial.println("BLE Central - LED control");
+
+  // start scanning for peripherals
+  BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1218");
+}
+
+void loop() {
+  // check if a peripheral has been discovered
+  BLEDevice peripheral = BLE.available();
+
+  if (peripheral) {
+    // discovered a peripheral, print out address, local name, and advertised service
+    Serial.print("Found ");
+    Serial.print(peripheral.address());
+    Serial.print(" '");
+    Serial.print(peripheral.localName());
+    Serial.print("' ");
+    Serial.print(peripheral.advertisedServiceUuid());
+    Serial.println();
+
+    if (peripheral.localName() != "LEDCallback") {
+      return;
+    }
+
+    // stop scanning
+    BLE.stopScan();
+
+    controlLed(peripheral);
+
+    // peripheral disconnected, start scanning again
+    BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1218");
+  }
+}
+
+void controlLed(BLEDevice peripheral) {
+  // connect to the peripheral
+  Serial.println("Connecting ...");
+
+  if (peripheral.connect()) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Failed to connect!");
+    return;
+  }
+
+
+  // discover peripheral attributes
+  Serial.println("Discovering attributes ...");
+  if (peripheral.discoverAttributes()) {
+    Serial.println("Attributes discovered");
+  } else {
+    Serial.println("Attribute discovery failed!");
+    //peripheral.disconnect();
+    //return;
+  }
+    Serial.println(peripheral.characteristicCount());
+  // retrieve the LED characteristic
+  BLECharacteristic ledCharacteristic = peripheral.characteristic("19b10001-e8f2-537e-4f6c-d104768a1218");
+   
+  if (!ledCharacteristic) {
+    Serial.println("Peripheral does not have LED characteristic!");
+    peripheral.disconnect();
+    return;
+  } else if (!ledCharacteristic.canWrite()) {
+    Serial.println("Peripheral does not have a writable LED characteristic!");
+    peripheral.disconnect();
+    return;
+  }
+
+  while (peripheral.connected()) {
+    // while the peripheral is connected
+
+    // read the button pin
+    byte data[DATA_LENGTH];
+        ledCharacteristic.readValue(data,DATA_LENGTH);
+        if
+        Serial.println("DATA Start");
+        for(int i = 0; i < DATA_LENGTH; i++)
+                {
+                  Serial.println(data[i]);
+                }
+        Serial.println("DATA END");
+     
+        // button is released, write 0x00 to turn the LED off
+      }
+
+  Serial.println("Peripheral disconnected");
+}
